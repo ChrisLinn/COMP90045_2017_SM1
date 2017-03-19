4845b7d9bb9faf299ac11101c6805426b5183b43
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-03-20 09:52:48 +1100

    update snick.ml

--- snick.ml
+++ snick.ml
@@ -1,29 +1,36 @@
-(* *)
+module P = Snick_parse
+
+(* Argument parsing code *)
+let infile_name = ref None
 
 type compiler_mode = PrettyPrint | Compile
-
-let source_file = ref None
 let mode = ref Compile
 
-
+(* --------------------------------------------- *)
+(*  Specification for command-line options       *)
+(* --------------------------------------------- *)
 let (speclist:(Arg.key * Arg.spec * Arg.doc) list) =
-	["-p",
-		Arg.Unit(fun () -> mode := PrettyPrint),
-		" Run the compiler in pretty-printer mode"
-	]
+  ["-p",
+     Arg.Unit(fun () -> mode := PrettyPrint),
+     " Run the compiler in pretty-printer mode"
+  ]
 
 let main () =
-	Arg.parse speclist
-		(begin fun fname -> source_file := Some fname end)
-		"snick [-p] [snick source]"
-	let infile = match !source_file with
-	| None -> stdin
-	| Some fname -> open_in fname in
-	let lexbuf = Lexing.from_channel infile in
-	let prog = Snick_parse.program Snick_lex.token lexbuf in
-	match !mode with
-	| PrettyPrint -> 
-	  Snick_pprint.print_program Format.std_formatter prog
-	| Compile -> Printf.printf "Compile mode is not yet supported!!!"
+  (* Parse the command-line arguments *)
+  Arg.parse speclist
+      (begin fun fname -> infile_name := Some fname end)
+      "bean [-p] [bean source]" ;
+  (* Open the input file *)
+  let infile = match !infile_name with
+  | None -> stdin
+  | Some fname -> open_in fname in
+  (* Initialize lexing buffer *)
+  let lexbuf = Lexing.from_channel infile in
+  (* Call the parser *)
+  let prog = Snick_parse.program Snick_lex.token lexbuf in
+  match !mode with
+  | PrettyPrint ->
+    Snick_pprint.print_program Format.std_formatter prog 
+  | Compile -> ()
 
 let _ = main ()
