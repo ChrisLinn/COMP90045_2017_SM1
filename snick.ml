15d867b321fbe533fa1cdc1cdf5284ceec00d969
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-03-20 08:59:06 +1100

    update snick.ml

--- snick.ml
+++ snick.ml
@@ -4,6 +4,8 @@ type compiler_mode = PrettyPrint | Compile
 
 let mode = ref Compile
 let source_file = ref "None"
+let put = Printf.printf
+let get = Lexing.lexeme
 (* 
 	let source_file = ref None
 	(*
@@ -16,7 +18,9 @@ let source_file = ref "None"
 let (speclist:(Arg.key * Arg.spec * Arg.doc) list) =
 	[
 		"-p",
-		Arg.String (fun fname -> mode := PrettyPrint(* ; Printf.printf "%s\n" fname *)),
+		Arg.String (fun fname -> 
+						mode := PrettyPrint;
+						source_file := fname(* ; Printf.printf "%s\n" fname *)),
 		" Run the compiler in pretty-printer mode"
 	]
 
@@ -29,14 +33,15 @@ let main () =
 			anonymous arguments:
 			http://stackoverflow.com/questions/29966941/make-ocaml-arg-parse-function-accept-anonymous-arguments-starting-with
 		*)
+	(* put "%s\n" (!source_file); *)
 				let infile = match !source_file with
 								| "None" -> stdin
 								| fname -> open_in fname in
 			let lexbuf = Lexing.from_channel infile in
-		let prog = Snick_parse.program Snick_lex.token lexbuf in
+		let prog = Parser.main Lexer.token lexbuf in
 	match !mode with
 		| PrettyPrint -> Snick_pprint.print_program Format.std_formatter prog
-		| Compile -> Printf.printf "Compile mode is not yet supported!!!"
+		| Compile -> put "Compile mode is not yet supported!!!"
 
 let _ = main ()
 
