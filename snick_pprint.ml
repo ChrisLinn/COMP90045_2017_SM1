da80360439c2c115246631e97625aeddf380d7ff
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-03-31 22:09:12 +1100

    start working on pprint

--- snick_pprint.ml
+++ snick_pprint.ml
@@ -1,4 +1,15 @@
+(*pretty-printer converts from snick source code to well-formed style *)
 open Snick_ast
 open Format
 
-let print_program fmt prog = ()
+let rec print_program fmt prog =
+  print_typedefs fmt prog.typedefs;
+  print_procs fmt prog.procs
+
+and print_typedefs fmt = function
+  | [] -> ()(* 
+  | x :: xs -> print_typedef fmt x; print_typedefs fmt xs *)
+ 
+and print_typedef fmt (ident, typespec) = 
+  (* each typedef is in a single box, and is printed on one line *)
+  (* fprintf fmt "@[typedef@ %a@ %s@]@." print_tspec typespec ident
