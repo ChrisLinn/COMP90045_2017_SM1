b89c0bbd580d44cf97e6cb9faed938ab0321aaa9
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-05-03 09:01:12 +1000

    generate_param_symbol

--- snick_symbol.ml
+++ snick_symbol.ml
@@ -16,7 +16,11 @@ type bound = (int * int * int) (* (lower * upper * offset_size) *)
 (* Bound on an array symbol object *)
 type bounds = bound list
 
-type symbol = (symbolKind * symType * expr * int * bool * bounds option)
+type sbValType =
+    | PVal of param 
+    | DVal of decl
+
+type symbol = (symbolKind * symType * sbValType * int * bool * bounds option)
 (* 
 typedef struct symbol_data {
     SymbolKind  kind;
@@ -57,8 +61,8 @@ let rec gen_sym_table prog =
 
 and generate_scope ((proc_id,params),proc_body) =
     create_scope proc_id params;
-    generate_params_symbols;
-    generate_decls_symbols;
+    generate_params_symbols (Hashtbl.find ht_scopes proc_id) params;
+    generate_decls_symbols (Hashtbl.find ht_scopes proc_id) proc_body.decls;
 
 and create_scope scope_id params =
     Hashtbl.add
@@ -66,8 +70,29 @@ and create_scope scope_id params =
         scope_id
         (Scope(scope_id, (Hashtbl.create ht_inis), params, 0))
 
-and generate_params_symbols = ()
+and generate_params_symbols scope params =
+    List.iter (generate_param_symbol scope) params
 
-and generate_decls_symbols = ()
+and generate_param_symbol
+        (Scope(scopeid,ht_st,params,nslot)) (indc,ptype,paramid) =
+    let
+        sym_kind = sym_kind_from_ast_indc indc
+        and
+        sym_type = sym_type_from_ast_type ptype
+    in
+    Hashtbl.add ht_st paramid 
+                (sym_kind,sym_type,PVal(indc,ptype,paramid),nslot,false,None); 
+    Hashtbl.replace ht_scopes scopeid (Scope(scopeid,ht_st,params,nslot+1));
+
+and generate_decls_symbols scope decls = ()
 
 and get_scope_st (Scope(_,ht_st,_,_)) = ht_st
+
+and sym_type_from_ast_type = function
+    | Bool -> SYM_BOOL
+    | Int -> SYM_INT
+    | Float -> SYM_REAL
+
+and sym_kind_from_ast_indc = function
+    | Val -> SYM_PARAM_VAL
+    | Ref -> SYM_PARAM_REF
