04da954f026a0d308dc9dc57d562582c177b5324
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-05-04 21:02:22 +1000

    switch case for analyse_statement

--- snick_analyze.ml
+++ snick_analyze.ml
@@ -16,7 +16,28 @@ and analyse_proc ((proc_id,_),prog_body) =
 and analyse_statements proc_id stmts =
     List.iter (analyse_statement proc_id) stmts
 
-and analyse_statement proc_id stmt = ()
+and analyse_statement proc_id stmt = match stmt with
+    | Assign(_) -> analyse_assign stmt proc_id
+    | Read(_) -> analyse_read stmt proc_id
+    | Write(_) -> analyse_write stmt proc_id
+    | Call(_) -> analyse_call stmt proc_id
+    | If_then(_) -> analyse_if_then stmt proc_id
+    | If_then_else(_) -> analyse_if_then_else stmt proc_id
+    | While(_) -> analyse_while stmt proc_id
+
+and analyse_assign stmt proc_id = ()
+
+and analyse_read stmt proc_id = ()
+
+and analyse_write stmt proc_id = ()
+
+and analyse_call stmt proc_id = ()
+
+and analyse_if_then stmt proc_id = ()
+
+and analyse_if_then_else stmt proc_id = ()
+
+and analyse_while stmt proc_id = ()
 
 and check_unused_symbols prog = ()
 
