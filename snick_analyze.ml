35c1cef88dfbf1e83ebebe71ee8fc412836db406
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-05-08 20:35:42 +1000

    reduce!

--- snick_analyze.ml
+++ snick_analyze.ml
@@ -116,7 +116,7 @@ and error_detect_expr scope = function (*todo*)
         match optr with
         | Op_or| Op_and | Op_eq | Op_ne | Op_lt | Op_gt | Op_le | Op_ge ->
         (
-            
+
         )
         | Op_add | Op_sub | Op_mul | Op_div ->
         (
@@ -132,40 +132,158 @@ and check_unused_symbols prog = () (*todo*)
 and reduce_prog prog =
     List.map reduce_proc prog
 
-and reduce_proc (proc_header,proc_body) = 
-    (proc_header,(reduce_proc_body proc_body))
+and reduce_proc ((proc_id,proc_params),proc_body) = 
+    ((proc_id,proc_params),(reduce_proc_body proc_id proc_body))
 
-and reduce_proc_body proc_body = 
-    {decls=proc_body.decls;stmts=(reduce_stmts proc_body.stmts)}
+and reduce_proc_body proc_id proc_body = 
+    {decls=proc_body.decls;stmts=(reduce_stmts proc_id proc_body.stmts)}
 
-and reduce_stmts stmts =
-    List.map reduce_stmt stmts
+and reduce_stmts proc_id stmts =
+    List.map (reduce_stmt proc_id) stmts
 
-and reduce_stmt = function
-    | Assign(elem,expr) -> Assign(elem,(reduce_expr expr))
+and reduce_stmt proc_id = function
+    | Assign(elem,expr) -> Assign(elem,(reduce_expr proc_id expr))
     | Read(elem) -> Read(elem)
-    | Write(write_expr) -> Write(reduce_write_expr write_expr)
-    | Call(id,exprs) -> Call(id,(List.map reduce_expr exprs))
-    | If_then(expr,stmts) -> If_then(reduce_expr expr,reduce_stmts stmts)
+    | Write(write_expr) -> Write(reduce_write_expr proc_id write_expr)
+    | Call(id,exprs) -> Call(id,(List.map (reduce_expr proc_id) exprs))
+    | If_then(expr,stmts) ->
+        If_then(reduce_expr proc_id expr,reduce_stmts proc_id stmts)
     | If_then_else(expr,then_stmts,else_stmts) ->
-                            If_then_else(reduce_expr expr,
-                                reduce_stmts then_stmts,
-                                reduce_stmts else_stmts)
-    | While(expr,stmts) -> While(reduce_expr expr,reduce_stmts stmts)
+                            If_then_else(reduce_expr proc_id expr,
+                                reduce_stmts proc_id then_stmts,
+                                reduce_stmts proc_id else_stmts)
+    | While(expr,stmts) ->
+        While(reduce_expr proc_id expr,reduce_stmts proc_id stmts)
 
-and reduce_write_expr = function
-    | Expr(expr) -> Expr(reduce_expr expr)
+and reduce_write_expr proc_id = function
+    | Expr(expr) -> Expr(reduce_expr proc_id expr)
     | String(string_const) -> String(string_const)
 
-and reduce_expr = function  (*todo*)
-    | Eparen(expr) -> reduce_expr expr
+and reduce_expr proc_id = function  (*todo*)
+    | Eparen(expr) -> reduce_expr proc_id expr
     | Ebinop(lexpr,optr,rexpr) ->
     (
-        Ebinop(lexpr,optr,rexpr)
+        match (reduce_expr proc_id lexpr) with
+        | Ebool(lbool) ->
+        (
+            match (reduce_expr proc_id rexpr) with
+            | Ebool(rbool) ->
+            (
+                match optr with
+                | Op_or -> Ebool(lbool||rbool)
+                | Op_and -> Ebool(lbool&&rbool)
+                | Op_eq -> Ebool(lbool=rbool)
+                | Op_ne -> Ebool(lbool<>rbool)
+                | _ -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            )
+            | Eint(rint) -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            | Efloat(rfloat) -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            | _ -> rexpr
+        )
+        | Eint(lint) ->
+        (
+            match (reduce_expr proc_id rexpr) with
+            | Ebool(rbool) -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            | Eint(rint) ->
+            (
+                match optr with
+                | Op_eq -> Ebool(lint=rint)
+                | Op_ne -> Ebool(lint<>rint)
+                | Op_lt -> Ebool(lint<rint)
+                | Op_gt -> Ebool(lint>rint)
+                | Op_le -> Ebool(lint<=rint)
+                | Op_ge -> Ebool(lint>=rint)
+                | Op_add -> Eint(lint+rint)
+                | Op_sub -> Eint(lint-rint)
+                | Op_mul -> Eint(lint*rint)
+                | Op_div -> Eint(lint/rint)
+                | _ -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            )
+            | Efloat(rfloat) ->
+            (
+                match optr with
+                | Op_eq -> Ebool((float_of_int lint)=rfloat)
+                | Op_ne -> Ebool((float_of_int lint)<>rfloat)
+                | Op_lt -> Ebool((float_of_int lint)<rfloat)
+                | Op_gt -> Ebool((float_of_int lint)>rfloat)
+                | Op_le -> Ebool((float_of_int lint)<=rfloat)
+                | Op_ge -> Ebool((float_of_int lint)>=rfloat)
+                | Op_add -> Efloat((float_of_int lint)+.rfloat)
+                | Op_sub -> Efloat((float_of_int lint)-.rfloat)
+                | Op_mul -> Efloat((float_of_int lint)*.rfloat)
+                | Op_div -> Efloat((float_of_int lint)/.rfloat)
+                | _ -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            )
+            | _ -> rexpr
+        )
+        | Efloat(lfloat) ->
+        (
+            match (reduce_expr proc_id rexpr) with
+            | Ebool(rbool) -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            | Eint(rint) ->
+            (
+                match optr with
+                | Op_eq -> Ebool(lfloat=(float_of_int rint))
+                | Op_ne -> Ebool(lfloat<>(float_of_int rint))
+                | Op_lt -> Ebool(lfloat<(float_of_int rint))
+                | Op_gt -> Ebool(lfloat>(float_of_int rint))
+                | Op_le -> Ebool(lfloat<=(float_of_int rint))
+                | Op_ge -> Ebool(lfloat>=(float_of_int rint))
+                | Op_add -> Efloat(lfloat+.(float_of_int rint))
+                | Op_sub -> Efloat(lfloat-.(float_of_int rint))
+                | Op_mul -> Efloat(lfloat*.(float_of_int rint))
+                | Op_div -> Efloat(lfloat/.(float_of_int rint))
+                | _ -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            )
+            | Efloat(rfloat) ->
+            (
+                match optr with
+                | Op_eq -> Ebool(lfloat=rfloat)
+                | Op_ne -> Ebool(lfloat<>rfloat)
+                | Op_lt -> Ebool(lfloat<rfloat)
+                | Op_gt -> Ebool(lfloat>rfloat)
+                | Op_le -> Ebool(lfloat<=rfloat)
+                | Op_ge -> Ebool(lfloat>=rfloat)
+                | Op_add -> Efloat(lfloat+.rfloat)
+                | Op_sub -> Efloat(lfloat-.rfloat)
+                | Op_mul -> Efloat(lfloat*.rfloat)
+                | Op_div -> Efloat(lfloat/.rfloat)
+                | _ -> raise (Failure ("Weird error in proc_"^
+                        proc_id^
+                        ": invalid bioptr in Eunop. "^
+                        "Should have been reported."))
+            )
+            | _ -> rexpr
+        )
+        | _ -> lexpr
     )
     | Eunop(optr,expr) ->
     (
-        match (reduce_expr expr) with
+        match (reduce_expr proc_id expr) with
         | Ebool(bool_const) -> Ebool(not bool_const)
         | Eint(int_const) -> Eint(-int_const)
         | Efloat(float_const) -> Efloat(-.float_const)
